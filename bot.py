b3761688388a558d4b6bec91657485798a6ed144
kyamegel <72809087+kyamegel@users.noreply.github.com>
2025-10-10 19:22:14 +0800

    fixed bot.py

--- bot.py
+++ bot.py
@@ -3,23 +3,20 @@ from discord.ext import commands
 from dotenve import load_dotenv
 import os
 
-load dotenv()
+load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
-inents.message_content = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 for filename in os.listdir("./cogs"):
-	if filename.endswith(".py:):
+	if filename.endswith(".py"):
 		bot.load_extension(f"cogs.{filename[:-3]}")
 
 @bot.event
 async def on_ready():
 	print(f"✅ Logged in as {bot.user}")
-    await bot.tree.sync()
-    print("✅ Slash commands synced")
 
 bot.run(TOKEN)
-
